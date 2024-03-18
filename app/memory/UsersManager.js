e18a37844c4d49b728d1da3166038432a17c0f2e
Ariel Endrizzi <104076288+arielend@users.noreply.github.com>
2024-03-18 15:32:01 -0300

    add fileSystem persistence and error handling (#5)

--- app/memory/UsersManager.js
+++ app/memory/UsersManager.js
@@ -25,7 +25,67 @@ class UsersManager {
     }
 
     read () {
-        (UsersManager.#users.length > 0) ? console.log(UsersManager.#users) : console.log('Aún no hay usuarios registrados')
+        try {
+            if(UsersManager.#users.length > 0) {
+                console.log('Todos los usuarios: ', UsersManager.#users)
+            } else {
+                const error = new Error('No hay usuarios que mostrar')
+                throw error
+            }
+            
+        } catch (error) {
+            console.log(`An error has ocurred while calling read method: ${error}`)
+        }        
+    }
+
+    readOne (id) {
+        try {
+
+            if(!id) {
+                const error = new Error('The ID argument is required while calling de readOne method.')
+                throw error
+            } else {
+
+                if(UsersManager.#users.length > 0) {
+                    const one = UsersManager.#users.find ( user => user.id === id)
+                    one ? console.log('Usuario encontrado: ', one) : console.log('No se ha encontrado el usuario')
+                } else {
+                    console.log('No hay usuarios guardados.')
+                }                
+            }
+            
+        } catch (error) {
+            console.log(`An error has ocurred while calling readOne method: ${error}`)
+        }
+    }
+
+    destroyOne (id) {
+        try {
+
+            if(!id) {
+                const error = new Error('The ID argument is required while calling de destroyOne method.')
+                throw error
+            } else {
+
+                if(UsersManager.#users.length > 0) {
+                    const one = UsersManager.#users.find ( user => user.id === id)
+                    if(one){
+
+                        UsersManager.#users = UsersManager.#users.filter(user => user.id !== id)
+                        console.log('se ha eliminado el usuario: ', one)
+
+                    } else {
+                        console.log('No se ha encontrado el usuario')
+                    }
+                    
+                } else {
+                    console.log('No hay usuarios guardados.')
+                }
+            }
+            
+        } catch (error) {
+            console.log(`An error has ocurred while calling destroyOne method: ${error}`)            
+        }
     }
 }
 
@@ -55,4 +115,16 @@ const user4 = manager.create({
     password: ''
 })
 
-console.log(manager.read())
+manager.read()
+
+manager.readOne(2)
+
+manager.readOne(7)
+
+manager.destroyOne(6)
+
+manager.destroyOne(2)
+
+setTimeout(()=>{
+    manager.read()
+}, 2000)
