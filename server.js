c06252cb671fd8225b89e6590ac8d3f41427cf4c
Natalia Peripolli <nsperipolli@gmail.com>
2024-04-10 14:04:05 -0300

    se agregan vistas de usuario y login

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
-import express from 'express'
-import morgan from 'morgan'
-import indexRouter from './src/routers/index.router.js'
-import errorHandler from './src/middlewares/errorHandler.js'
-import pathHandler from './src/middlewares/pathHandler.js'
+    import express from 'express'
+    import { createServer } from 'http'
+    import { Server } from 'socket.io'
+    import morgan from 'morgan'
+    import { engine } from 'express-handlebars'
+    import indexRouter from './src/routers/index.router.js'
+    import socketCb from './src/routers/index.socket.js'
+    import errorHandler from './src/middlewares/errorHandler.js'
+    import pathHandler from './src/middlewares/pathHandler.js'
+    import __dirname from './utils.js'
 
-// Server
-const server = express()
-const port = 8080
-const ready = () => { console.log(`Server ready on http://localhost:${port}/`) }
 
-server.listen(port, ready)
+    // Server
+    const server = express()
+    const port = 8080
+    const ready = () => { console.log(`Server ready on http://localhost:${port}/`) }
+    const nodeServer = createServer(server)
+    nodeServer.listen(port, ready);
+    const socketServer = new Server(nodeServer)
+    socketServer.on("connection", socketCb)
+    
 
-// Middlewares
-server.use(express.urlencoded({ extended: true }))
-server.use(express.json())
-server.use(morgan("dev"))
+    // Template engine
+    server.engine('handlebars', engine())
+    server.set('view engine', 'handlebars')
+    server.set('views', __dirname+'/src/views')
 
-// Router
-server.use('/', indexRouter)
+    // Middlewares
+    server.use(express.urlencoded({ extended: true }))
+    server.use(express.json())
+    server.use(morgan("dev"))
 
-//Error and Path handling
-server.use(errorHandler)
-server.use(pathHandler)
+    // Router
+    server.use('/', indexRouter)
+
+    //Error and Path handling
+    server.use(errorHandler)
+    server.use(pathHandler)
