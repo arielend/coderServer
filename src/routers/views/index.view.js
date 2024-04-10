1c60942bcd1e7a95c6cb97bdbb4249351dbbc7b6
nsperi <94202231+nsperi@users.noreply.github.com>
2024-04-10 14:05:31 -0300

    Naty (#16)
    * routers
    
    * se agregan vistas de usuario y login

--- src/routers/views/index.view.js
+++ src/routers/views/index.view.js
@@ -1,8 +1,15 @@
-import { Router } from 'express'
-import productsRouter from '../api/products.api.js'
+import { Router } from 'express';
+import usersRouter from './users.view.js';
 
-const viewsRouter = Router()
+const viewsRouter = Router();
 
-viewsRouter.use('/products', productsRouter)
+viewsRouter.use('/users', usersRouter);
+viewsRouter.get('/', (_request, response, _next) => {
+    try{
+        return response.render('index', {title: 'CompuMundoHiperMegaRed'})
+    }catch (error) {
+        return _next(error)
+    }
+})
 
-export default viewsRouter
+export default viewsRouter;
