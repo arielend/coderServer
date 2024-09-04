7d30c25ba0f7fe19a7d44aded48243bae5b23a0b
arielend <arielendrizzi78@gmail.com>
2024-09-04 17:20:54 -0300

    cooking

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -21,7 +21,7 @@ class SessionsController {
                 signed: true,
                 httpOnly: true,
                 secure: true,
-                sameSite: 'None',
+                sameSite: 'Lax',
                 maxAge: 60 * 60 * 1000 // 1 hora de vida
                 }
             )
@@ -37,7 +37,7 @@ class SessionsController {
             response.cookie('user', JSON.stringify(user),{
                 httpOnly: false,
                 secure: true,
-                sameSite: 'None',
+                sameSite: 'Lax',
                 maxAge: 60 * 60 * 1000 // 1 hora de vida
             })
 
