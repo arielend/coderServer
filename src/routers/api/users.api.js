344e6f5c8270e5de9f0f39b17dfc49af02d0a2d4
arielend <arielendrizzi78@gmail.com>
2024-09-04 21:53:19 -0300

    cookieBack

--- src/routers/api/users.api.js
+++ src/routers/api/users.api.js
@@ -3,8 +3,8 @@ import { readOne, update } from '../../controllers/users.controller.js'
 
 class UsersRouter extends CustomRouter {
     init(){
-        this.readOne('/:id', ['ADMIN', 'CUSTOMER', 'PREM'], readOne)
         this.readOne('/inSession', ['ADMIN', 'CUSTOMER', 'PREM'], readOne)
+        this.readOne('/:id', ['ADMIN', 'CUSTOMER', 'PREM'], readOne)
         this.update('/', ['ADMIN', 'CUSTOMER', 'PREM'], update)
     }
 }
